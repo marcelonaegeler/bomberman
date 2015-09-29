931294bd71edb97cb61021d644aa822fe76744be
Marcelo A Naegeler <marcelo.andre.naegeler@gmail.com>
2015-09-29 18:04:06 -0300

    Fixed blocks now have their own color. Code improvement

--- script.js
+++ script.js
@@ -1,27 +1,87 @@
 var app = (function() {
   var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
   
   var posX = 0
     , posY = 0
     , interval = 50
-    , animating = false;
+    , animating = false
+    , playerColor = '#fff'
+    , mapColor = '#666'
+    ;
+  var draw = (function() {
+    var context = canvas.getContext('2d');
 
-  var canvasProps = {
-    width: canvas.width
-    , height: canvas.height
-  };
+    var canvasProps = {
+      width: canvas.width
+      , height: canvas.height
+    };
 
-  var clearCanvas = function() {
-    context.clearRect(0, 0, canvasProps.width, canvasProps.height);
+    var clearCanvas = function() {
+      context.clearRect(0, 0, canvasProps.width, canvasProps.height);
+    };
+
+    var map = function() {
+      context.fillStyle = mapColor;
+      context.fillRect(50, 50, 50, 50);
+      context.fillRect(150, 50, 50, 50);
+
+    };
+
+    var player = function(x, y) {
+      context.fillStyle = playerColor;
+      context.fillRect(posX, posY, 50, 50);
+
+      var destX = posX + x;
+      var destY = posY + y;
+
+      setTimeout(function() {
+        if(destX != posX || destY != posY) {
+          if(x > 0) posX += 5;
+          else if(x < 0) posX -= 5;
+          if(y > 0) posY += 5;
+          else if(y < 0) posY -= 5;
+
+          animating = true;
+          game(destX - posX, destY - posY);
+        } else {
+          animating = false;
+        }
+      });
+    };
+
+    var game = function(x, y) {
+      clearCanvas();
+      context.beginPath();
+
+      map();
+      player(x, y);
+
+      context.closePath();
+    };
+
+    return {
+      game: game
+    };
+  }());
+
+
+
+/*
+  var drawMap = function(cb) {
+    context.fillStyle = '#666';
+    context.rect(50, 50, 50, 50);
+    cb();
   };
 
   var drawRect = function(x, y) {
     clearCanvas();
     context.beginPath();
     context.rect(posX, posY, 50, 50);
-    context.closePath();
-    context.fill();
+
+    drawMap(function() {
+      context.closePath();
+      context.fill();
+    });
 
     var destX = posX + x;
     var destY = posY + y;
@@ -40,7 +100,7 @@ var app = (function() {
       }
     }, 8);
   };
-
+*/
   var doMovement = function(key) {
     var x = 0
       , y = 0;
@@ -52,7 +112,8 @@ var app = (function() {
       x -= interval;
     else if(key == 76 || key == 39)
       x += interval;
-    drawRect(x, y);
+    //drawRect(x, y);
+    draw.game(x, y);
   };
 
   var keyboardHandle = function() {
@@ -67,7 +128,8 @@ var app = (function() {
 
   // Init function
   (function() {
-    drawRect(0, 0);
+    draw.game(0, 0);
+    //drawRect(0, 0);
     keyboardHandle();
   }());
   
